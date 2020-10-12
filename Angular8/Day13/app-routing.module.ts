ddf0ed943d6380125eb961c095fcb17b542bf1c0
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-10-12 17:12:27 +0530

    Day13

---
+++ Angular8/Day13/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FollowerComponent } from './follower/follower.component';
+import { HomeComponent } from './home/home.component';
+import { MyfollowerComponent } from './myfollower/myfollower.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
+import { QueryparameterComponent } from './queryparameter/queryparameter.component';
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  { path:'page1', component:Page1Component },
+  {path:'page2',component:Page2Component},
+  {path:'page3',component:Page3Component},
+  {path:'follower',component:FollowerComponent},
+  {path:'queryparam',component:QueryparameterComponent 
+  },
+  {path:"myfollower",component:MyfollowerComponent},
+  {path:"**",component:NotfoundComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

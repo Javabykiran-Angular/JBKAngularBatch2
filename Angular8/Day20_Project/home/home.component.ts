7f2110ff3b0970a7c7e075fa708fc143d8c0100d
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2020-11-10 17:26:09 +0530

    Day20

---
+++ Angular8/Day20_Project/home/home.component.ts
@@ -0,0 +1,243 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  tempEmpdata:any[]=[
+    {
+      id:1,
+      name:'sumit',
+      departmentit:'IT',
+      status:'Active',
+      phoneno:9960556397,
+      country:{
+        cid:1,
+        cname:'India'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Sumit',
+      updateddtm:'22-10-2020',
+      updatedby:'Sumit'
+
+    },
+    {
+      id:2,
+      name:'Kiran',
+      departmentit:'Marketing',
+      status:'Active',
+      phoneno:9960558956,
+      country:{
+        cid:1,
+        cname:'India'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Sumit1',
+      updateddtm:'22-10-2020',
+      updatedby:'Sumit'
+
+    }, {
+      id:3,
+      name:'Spruha',
+      departmentit:'Administrative',
+      status:'InActive',
+      phoneno:8087556397,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Sumit',
+      updateddtm:'22-10-2020',
+      updatedby:'Sumit'
+
+    },
+    {
+      id:4,
+      name:'Amit',
+      departmentit:'IT',
+      status:'InActive',
+      phoneno:9960789397,
+      country:{
+        cid:3,
+        cname:'Germany'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Kiran',
+      updateddtm:'22-10-2020',
+      updatedby:'Kiran'
+
+    },
+    {
+      id:5,
+      name:'Nilam',
+      departmentit:'Account',
+      status:'Active',
+      phoneno:8460556397,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Sumit',
+      updateddtm:'22-10-2020',
+      updatedby:'Kiran'
+
+    },
+    {
+      id:6,
+      name:'Pratiksha',
+      departmentit:'Development',
+      status:'Active',
+      phoneno:9960556032,
+      country:{
+        cid:3,
+        cname:'Germany'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Sumit',
+      updateddtm:'22-10-2020',
+      updatedby:'Sumit'
+
+    },
+    {
+      id:7,
+      name:'XYZ',
+      departmentit:'Networking',
+      status:'Suspend',
+      phoneno:9978556032,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },
+    {
+      id:7,
+      name:'XYZ',
+      departmentit:'Networking',
+      status:'Suspend',
+      phoneno:9978556032,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },{
+      id:7,
+      name:'XYZ',
+      departmentit:'Networking',
+      status:'Suspend',
+      phoneno:9978556032,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },{
+      id:7,
+      name:'XYZ',
+      departmentit:'Networking',
+      status:'Suspend',
+      phoneno:9978556032,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },
+    {
+      id:88,
+      name:'XYZ1',
+      departmentit:'Networking',
+      status:'Active',
+      phoneno:9978556032,
+      country:{
+        cid:2,
+        cname:'USA'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },
+    {
+      id:77,
+      name:'PQR',
+      departmentit:'Networking',
+      status:'Suspend',
+      phoneno:9978556038,
+      country:{
+        cid:1,
+        cname:'India'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    },
+    {
+      id:70,
+      name:'XYZ12',
+      departmentit:'Marketing',
+      status:'InActive',
+      phoneno:9978556032,
+      country:{
+        cid:1,
+        cname:'India'
+      },
+      createddtm:'22-10-2020',
+      createdby:'Admin',
+      updateddtm:'22-10-2020',
+      updatedby:'Admin'
+
+    }
+  ];
+  constructor(private route:Router) { }
+
+  ngOnInit() {
+  }
+
+  onRadioClick(item){
+    console.log(item);
+  }
+
+  onAddEmpolyee(){
+    this.route.navigate(['/addEmployee']);
+  }
+  onUpdate(){
+
+  }
+  onDelete(){
+
+  }
+  onSuspend(){
+
+  }
+  onChangeStatus(){
+
+  }
+
+}
